ed17d3ead9c7774c801db8589af174cafb2361c7
Emilien AUFAUVRE <emilien.aufauvre@live.fr>
2020-05-05 00:27:34 +0200

    Amélioration de la structure de l'UI. Début de développement du menu d'introduction.

--- src/azul/view/ui/UIPanel.java
+++ src/azul/view/ui/UIPanel.java
@@ -7,20 +7,17 @@ import java.awt.*;
 
 public class UIPanel extends JPanel
 {
-    // UI states.
-    public enum State { MAIN_MENU, IN_GAME, SETTINGS }
-
     // Background colors.
     public static final Color BG_MAIN_MENU = Color.DARK_GRAY ;
-    public static final Color BG_IN_GAME = new Color(0, 0, 0, 0) ;
-    public static final Color BG_SETTINGS = Color.DARK_GRAY ;
+    public static final Color BG_CREDITS = Color.RED ;
+    public static final Color BG_IN_GAME = Color.RED ;
+    public static final Color BG_SETTINGS = Color.GREEN ;
 
     // Root ref.
     private Display mDisplay ;
-    // Current state.
-    private State mState ;
     // Screens.
     private MainMenu mMainMenu ;
+    private Credits mCredits ;
     private InGame mInGame ;
     private Settings mSettings ;
 
@@ -37,16 +34,17 @@ public class UIPanel extends JPanel
 
     private void initialize()
     {
+        setLayout(new BorderLayout(0, 0)) ;
+
         mMainMenu = new MainMenu(mDisplay) ;
+        mCredits = new Credits(mDisplay) ;
         mInGame = new InGame(mDisplay) ;
         mSettings = new Settings(mDisplay) ;
-
-        add(mMainMenu) ;
-        add(mInGame) ;
-        add(mSettings) ;
-
-        // Start in the main menu.
-        onGoMainMenu() ;
+        // Impossible to find a way of stacking the components...
+        add(mMainMenu, BorderLayout.SOUTH) ;
+        add(mCredits, BorderLayout.WEST) ;
+        add(mInGame, BorderLayout.CENTER) ;
+        add(mSettings, BorderLayout.NORTH) ;
     }
 
     /**
@@ -54,7 +52,14 @@ public class UIPanel extends JPanel
      */
     public void onGoMainMenu()
     {
-        mState = State.MAIN_MENU ;
+        setVisibilities() ;
+    }
+
+    /**
+     * Called when user selects the credits option.
+     */
+    public void onGoCredits()
+    {
         setVisibilities() ;
     }
 
@@ -63,10 +68,7 @@ public class UIPanel extends JPanel
      */
     public void onGoInGame()
     {
-        mState = State.IN_GAME ;
         setVisibilities() ;
-
-        mDisplay.startGame(4) ;
     }
 
     /**
@@ -74,26 +76,22 @@ public class UIPanel extends JPanel
      */
     public void onGoSettings()
     {
-        mState = State.SETTINGS ;
         setVisibilities() ;
     }
 
     private void setVisibilities()
     {
         mMainMenu.setVisible(false) ;
+        mCredits.setVisible(false) ;
         mInGame.setVisible(false) ;
         mSettings.setVisible(false) ;
 
-        switch (mState)
+        switch (mDisplay.getState())
         {
             case MAIN_MENU : mMainMenu.setVisible(true) ; break ;
+            case CREDITS : mCredits.setVisible(true) ; break ;
             case IN_GAME : mInGame.setVisible(true) ; break ;
             case SETTINGS : mSettings.setVisible(true) ;
         }
-    }
-
-    public State getState()
-    {
-        return mState ;
     }
 }
